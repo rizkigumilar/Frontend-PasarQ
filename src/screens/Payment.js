8e29a78d5d65a5f06be659fcc7b2e2024acbbfc4
Rezha <rezharatulangi@gmail.com>
2019-08-27 23:18:53 +0700

    add profileToko dan ListBarang

--- src/screens/Payment.js
+++ src/screens/Payment.js
@@ -1,158 +1,158 @@
-import React, { Component, Fragment } from 'react';
-import { Icon } from 'native-base';
+import React, {Component, Fragment} from 'react';
+import {Icon} from 'native-base';
 import {
-    StyleSheet,
-    Text,
-    View,
-    FlatList,
-    Image,
-    TouchableOpacity,
-    TextInput,
-    ScrollView,
-    Alert,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
 } from 'react-native';
-import { withNavigation } from 'react-navigation';
+import {withNavigation} from 'react-navigation';
 
 class Payment extends Component {
-    render() {
-        return (
-            <Fragment>
-                <View style={styles.header}>
-                    <View style={styles.back}>
-                        <Icon
-                            name="arrow-back"
-                            color="#000000"
-                            size={32}
-                            style={styles.menuIcon}
-                            onPress={() => this.props.navigation.navigate('Cart')}
-                        />
-                    </View>
-                    <View style={styles.label}>
-                        <Text>Checkout</Text>
-                    </View>
-                </View>
-                <View style={styles.containerProfile}>
-                    <Text style={{ marginBottom: 5 }}>Rezha Riansyah R</Text>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Icon name="pin" size={32} />
-                        <Text style={styles.text}>
-                            Jl. Selokan Mataram Gg. Nakula No. 303 C, Sinduaji, Mlati, Kutu
-                            Dukuh, Sinduadi, Sleman, Kabupaten Sleman, Daerah Istimewa
-                            Yogyakarta 83239
+  render() {
+    return (
+      <Fragment>
+        <View style={styles.header}>
+          <View style={styles.back}>
+            <Icon
+              name="arrow-back"
+              color="#000000"
+              size={32}
+              style={styles.menuIcon}
+              onPress={() => this.props.navigation.navigate('Cart')}
+            />
+          </View>
+          <View style={styles.label}>
+            <Text>Checkout</Text>
+          </View>
+        </View>
+        <View style={styles.containerProfile}>
+          <Text style={{marginBottom: 5}}>Rezha Riansyah R</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Icon name="pin" size={32} />
+            <Text style={styles.text}>
+              Jl. Selokan Mataram Gg. Nakula No. 303 C, Sinduaji, Mlati, Kutu
+              Dukuh, Sinduadi, Sleman, Kabupaten Sleman, Daerah Istimewa
+              Yogyakarta 83239
             </Text>
-                    </View>
-                    <View style={{ flexDirection: 'row', marginTop: 5 }}>
-                        <Icon name="call" size={32} />
-                        <Text style={styles.text}>0198239082398398</Text>
-                    </View>
-                    <View style={{ flexDirection: 'row', marginTop: 5 }}>
-                        <Icon name="mail" size={32} />
-                        <Text style={styles.text}>rezhariansyah@gmail.com</Text>
-                    </View>
-                </View>
-                <View style={styles.containerDelivery}>
-                    <Text style={{ textAlign: 'center' }}>Pilihan Pengiriman</Text>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            marginTop: 10,
-                            justifyContent: 'space-between',
-                            marginBottom: 20,
-                        }}>
-                        <TouchableOpacity
-                            style={styles.delivery}
-                            onPress={() => this.onPress}>
-                            <Text style={{ textAlign: 'center' }}> Express </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.delivery}
-                            onPress={() => this.onPress}>
-                            <Text style={{ textAlign: 'center' }}> To Day </Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            marginHorizontal: 20,
-                        }}>
-                        <Text>Sub Total (5 items)</Text>
-                        <Text style={{ color: 'red' }}>Rp. 50000</Text>
-                    </View>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            marginHorizontal: 20,
-                        }}>
-                        <Text>Biaya Pengiriman</Text>
-                        <Text style={{ color: 'red' }}>Rp. 7000</Text>
-                    </View>
-                </View>
-                <View style={{ justifyContent: 'flex-end', flex: 1 }}>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            marginHorizontal: 20,
-                            marginTop: 40,
-                            marginBottom: 20,
-                        }}>
-                        <Text style={{ fontSize: 20, color: 'red' }}>Total : Rp. 7000</Text>
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={() => this.onPress}>
-                            <Text style={{ color: 'white' }}> Buat Pesanan </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Fragment>
-        );
-    }
+          </View>
+          <View style={{flexDirection: 'row', marginTop: 5}}>
+            <Icon name="call" size={32} />
+            <Text style={styles.text}>0198239082398398</Text>
+          </View>
+          <View style={{flexDirection: 'row', marginTop: 5}}>
+            <Icon name="mail" size={32} />
+            <Text style={styles.text}>rezhariansyah@gmail.com</Text>
+          </View>
+        </View>
+        <View style={styles.containerDelivery}>
+          <Text style={{textAlign: 'center'}}>Pilihan Pengiriman</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginTop: 10,
+              justifyContent: 'space-between',
+              marginBottom: 20,
+            }}>
+            <TouchableOpacity
+              style={styles.delivery}
+              onPress={() => this.onPress}>
+              <Text style={{textAlign: 'center'}}> Express </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.delivery}
+              onPress={() => this.onPress}>
+              <Text style={{textAlign: 'center'}}> To Day </Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginHorizontal: 20,
+            }}>
+            <Text>Sub Total (5 items)</Text>
+            <Text style={{color: 'red'}}>Rp. 50000</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginHorizontal: 20,
+            }}>
+            <Text>Biaya Pengiriman</Text>
+            <Text style={{color: 'red'}}>Rp. 7000</Text>
+          </View>
+        </View>
+        <View style={{justifyContent: 'flex-end', flex: 1}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginHorizontal: 20,
+              marginTop: 40,
+              marginBottom: 20,
+            }}>
+            <Text style={{fontSize: 20, color: 'red'}}>Total : Rp. 7000</Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.onPress}>
+              <Text style={{color: 'white'}}> Buat Pesanan </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Fragment>
+    );
+  }
 }
 
 export default withNavigation(Payment);
 
 const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        height: 60,
-    },
-    back: {
-        justifyContent: 'center',
-        marginHorizontal: 20,
-    },
-    menuIcon: {
-        width: 20,
-        height: 30,
-    },
-    label: {
-        justifyContent: 'center',
-    },
-    containerProfile: {
-        marginHorizontal: 20,
-        borderBottomWidth: 3,
-        borderBottomColor: 'red',
-        paddingBottom: 10,
-    },
-    text: {
-        marginHorizontal: 13,
-    },
-    containerDelivery: {
-        marginHorizontal: 20,
-        marginVertical: 20,
-    },
-    delivery: {
-        height: 80,
-        width: '48%',
-        borderWidth: 2,
-        borderColor: 'red',
-        justifyContent: 'center',
-        borderRadius: 10,
-    },
-    button: {
-        alignItems: 'center',
-        backgroundColor: '#C30F42',
-        padding: 10,
-    },
+  header: {
+    flexDirection: 'row',
+    height: 60,
+  },
+  back: {
+    justifyContent: 'center',
+    marginHorizontal: 20,
+  },
+  menuIcon: {
+    width: 20,
+    height: 30,
+  },
+  label: {
+    justifyContent: 'center',
+  },
+  containerProfile: {
+    marginHorizontal: 20,
+    borderBottomWidth: 3,
+    borderBottomColor: 'red',
+    paddingBottom: 10,
+  },
+  text: {
+    marginHorizontal: 13,
+  },
+  containerDelivery: {
+    marginHorizontal: 20,
+    marginVertical: 20,
+  },
+  delivery: {
+    height: 80,
+    width: '48%',
+    borderWidth: 2,
+    borderColor: 'red',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#008000',
+    padding: 10,
+  },
 });
---
+++ src/screens/ProfileToko.js
@@ -0,0 +1,140 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, Image, View} from 'react-native';
+import {
+  Form,
+  Label,
+  Item,
+  Input,
+  Container,
+  Header,
+  Content,
+  Button,
+} from 'native-base';
+import {withNavigation} from 'react-navigation';
+
+class ProfileToko extends Component {
+  state = {
+    token: null,
+    email: '',
+    name: '',
+    id_user: '',
+  };
+
+  render() {
+    return (
+      <Container>
+        {/* <NavigationEvents
+          onWillFocus={() =>
+            AsyncStorage.getItem("name").then(value => {
+              this.setState({ name: value });
+            })
+          }
+        />
+        <NavigationEvents
+          onWillFocus={() =>
+            AsyncStorage.getItem("token").then(value => {
+              this.setState({ token: value });
+            })
+          }
+        />
+        <NavigationEvents
+          onWillFocus={() =>
+            AsyncStorage.getItem("email").then(value => {
+              this.setState({ email: value });
+            })
+          }
+        /> */}
+        <Content>
+          <View style={styles.container}>
+            <Image
+              style={styles.profileImage}
+              source={require('../assets/group.png')}
+            />
+          </View>
+          <Form style={styles.formInput}>
+            {this.state.token == null ? (
+              <View>
+                <Item inlineLabel>
+                  <Label>Nama Toko :</Label>
+                  <Input />
+                </Item>
+                <Item inlineLabel>
+                  <Label>Email :</Label>
+                  <Input />
+                </Item>
+                <Item inlineLabel last>
+                  <Label>Location :</Label>
+                  <Input />
+                </Item>
+              </View>
+            ) : (
+              <View>
+                <Item inlineLabel>
+                  <Label>Full Name : {this.state.name}</Label>
+                  <Input />
+                </Item>
+                <Item inlineLabel>
+                  <Label>Email : {this.state.email}</Label>
+                  <Input />
+                </Item>
+                <Item inlineLabel last>
+                  <Label>New Password :</Label>
+                  <Input />
+                </Item>
+              </View>
+            )}
+          </Form>
+          <View style={styles.save}>
+            <Button
+              style={styles.buttonSave}
+              rounded
+              info
+              onPress={() => this.props.navigation.navigate('BarangToko')}>
+              <Text style={{color: "white"}}>Lihat Jenis Barang</Text>
+            </Button>
+          </View>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+export default withNavigation(ProfileToko);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 45,
+  },
+  profileImage: {
+    width: 180,
+    height: 180,
+  },
+  text: {
+    fontSize: 30,
+  },
+  formInput: {
+    marginHorizontal: 10,
+  },
+  buttonSave: {
+    width: 150,
+    justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+    margin: 8,
+    marginTop: 0,
+    backgroundColor: "#008000"
+  },
+  save: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 60,
+    flexDirection: 'row',
+    marginHorizontal: 100,
+  },
+});
