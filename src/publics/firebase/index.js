41f0b8e915d0f7eac54568875ec9769814674704
Rezha <rezharatulangi@gmail.com>
2019-08-29 12:27:52 +0700

    firebase config

---
+++ src/publics/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBPT-ViexNMagJ4Fm11vKs2TpSjW1X8s2o',
+  authDomain: 'pasarq-bad37.firebaseapp.com',
+  databaseURL: 'https://pasarq-bad37.firebaseio.com',
+  projectId: 'pasarq-bad37',
+  storageBucket: '',
+  messagingSenderId: '235717198961',
+  appId: '1:235717198961:web:870200f34c902069',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
